5fd3c7109b30524429142da6c28e1bce83ac4d35
ramankala <rs.kala@hotmail.com>
2021-04-13 00:09:04 -0400

    Update Node.py

--- startA3/Node.py
+++ startA3/Node.py
@@ -11,20 +11,20 @@ class Node:
         # you implement the rest of constructor
         for i in range(num):
             if(i != ID):
-                if(costs[i] != 999):
+                if(costs[i] != self.ns.INFINITY):
                     print("\nTOLAYER2: " + "source: {} ".format(self.myID) + "dest: {} ".format(i) + "costs: ", end="")
                 for j in range(num):
-                    if(costs[i] != 999):
+                    if(costs[i] != self.ns.INFINITY):
                         print("{} ".format(costs[j]), end="")
                     if(i != j):
-                        self.distanceTable[i][j] = 999
-                if(costs[i] != 999):
+                        self.distanceTable[i][j] = self.ns.INFINITY
+                if(costs[i] != self.ns.INFINITY):
                     print("\n")
                 self.distanceTable[ID][i] = costs[i]
             else:
                 for j in range(num):
                     if(j != ID):
-                        self.distanceTable[j][i] = 999
+                        self.distanceTable[j][i] = self.ns.INFINITY
 
     def recvUpdate(self, pkt):
 
@@ -32,6 +32,7 @@ class Node:
 
         # you implement the rest of it
 
+        
         return
 
 
